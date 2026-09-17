dcf6cc04295b7b9f0777ddf8a3915a969f079c9b
Nataliia Prokopenko <nataliiaprokopenko@yahoo.com>
2026-09-17 12:22:11 +0200

    fix styling layout for mobile

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,7 +3,9 @@ import Header from "./Header";
 
 function Layout({children}) {
     return (<div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div className="hidden md:block">
         <Sidebar />
+        </div>
         <div className="flex flex-col flex-1">
             <Header />
             <div className="p-4 md:p-8">
